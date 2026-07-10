d4cb6cc11028ede7a927d46860c942eec456f3a1
flob57 <flob57@icloud.com>
2026-07-10 16:16:19 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,13 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    Promise.all([
-      caches.keys().then(names =>
-        Promise.all(
-          names.map(name => caches.delete(name))
-        )
-      ),
-
-      self.registration.unregister(),
-
-      self.clients.matchAll({
-        type: "window",
-        includeUncontrolled: true
-      }).then(clients => {
-        clients.forEach(client => {
-          client.navigate(client.url);
-        });
-      })
-    ])
+    caches.keys()
+      .then(names => Promise.all(
+        names.map(name => caches.delete(name))
+      ))
+      .then(() => self.registration.unregister())
   );
 });
